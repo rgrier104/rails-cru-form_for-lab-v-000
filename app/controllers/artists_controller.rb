f206feb78b9e4a8be5005bc0f800fb3fb2799304
Rachel Grier <rgrier104@gmail.com>
2019-09-19 18:44:53 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -18,6 +18,9 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     @artist.update(artist_params(:name, :bio))
 
+    redirect_to artist_path(@artist)
+  end
+
   private
 
   def artist_params(*args)
