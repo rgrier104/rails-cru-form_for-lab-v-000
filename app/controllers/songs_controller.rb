e1628b86e3a64f7fd25929d1e26c78c01ff3d815
Rachel Grier <rgrier104@gmail.com>
2019-09-19 18:54:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,33 @@
 class SongsController < ApplicationController
-  belongs_to :artist
-  belongs_to :genre
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
+
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = song.find(params[:id])
+  end
+
+  def update
+    @song = song.find(params[:id])
+    @song.update(song_params(:name, :bio))
+
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = song.find(params[:id])
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
