beeb85e1472c6df971cf10e8127e768a877bb099
Rachel Grier <rgrier104@gmail.com>
2019-09-19 18:42:53 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,4 +9,10 @@ class ArtistsController < ApplicationController
 
     redirect_to artist_path(@artist)
   end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
