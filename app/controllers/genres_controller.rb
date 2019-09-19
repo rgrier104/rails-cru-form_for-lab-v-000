3c669b693c6536103936c40e929c2e5ad9ea82e5
Rachel Grier <rgrier104@gmail.com>
2019-09-19 18:50:05 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,33 @@
 class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
